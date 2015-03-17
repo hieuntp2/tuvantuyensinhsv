90a543a0ce0efad80686231e616792536665b494
hieuntp2 <hieuntp2@gmail.com>
2015-03-17 20:31:34 +0700

    ơ

--- tuvantuyensinhsv.v2/tuvantuyensinhsv.v2/Scripts/AngularJs/homeindexscript.js
+++ tuvantuyensinhsv.v2/tuvantuyensinhsv.v2/Scripts/AngularJs/homeindexscript.js
@@ -1,7 +1,7 @@
 ﻿(function () {
     var app = angular.module('homepage', []);
 
-    app.controller('indexcontroller', ['$http', function ($http) {
+    app.controller('indexcontroller', ['$http', '$scope', function ($http, $scope) {
         var ctrll = this;
         this.posts = [];
 
@@ -17,14 +17,19 @@
                 ctrll.posts.push.apply(ctrll.posts,data);
             }).error(function () {
                 alert("Lỗi khi lấy dữ liệu questions");
-            });        }
+            });
+        }
 
-        this.init = function()
+        this.init = function ()
         {
             ctrll.getQuestions(0);
         }
 
-        this.postclick = function(id, loai)
+        $scope.addpost = function (index) {
+            this.getQuestions(index);
+        }
+
+        this.postclick = function (id, loai)
         {
             if(loai === 'b')
             {
@@ -34,5 +39,21 @@
                 window.location.href = "/questions/details/" + id;
             }
         }
+
+        $scope.getQuestions = function (index) {
+
+            $http.get("/baiviets/getNewPost?index=" + index).success(function (data) {
+                ctrll.posts.push.apply(ctrll.posts, data);
+            }).error(function () {
+                alert("Lỗi khi lấy dữ liệu bai viet");
+            });
+
+            $http.get("/questions/getquestion?index=" + index).success(function (data) {
+                ctrll.posts.push.apply(ctrll.posts, data);
+            }).error(function () {
+                alert("Lỗi khi lấy dữ liệu questions");
+            });
+        }
     }])
 })();
+
